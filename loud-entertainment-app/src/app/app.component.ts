2af6befb4917f65a6d91bad156571f3ecbbe206a
Muhammad Hafiz Abdul-Qayyum <muhammadaq93@yahoo.com>
2019-07-01 20:36:14 -0400

    First edit Transparent background navbar footer

---
+++ loud-entertainment-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <!-- header -->
+  <app-header></app-header>
+
+  <!-- testing the home page -->
+  <app-home></app-home>
+
+  <!-- routes will be rendered here -->
+  <router-outlet></router-outlet>
+
+  <!-- footer -->
+  <app-footer></app-footer>
+`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'loud-entertainment-app';
+}
